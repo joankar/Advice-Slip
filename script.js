f4d41e7a72ff734cc598c3ac8be470156f00a653
Raluca <muscalu.raluca@yahoo.com>
2023-11-11 14:58:44 +0200

    modif. js and css

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const ad_body = document.querySelector("#ad-body");
 const dice = document.querySelector("#dice");
 
 async function fetchData() {
-  const response = await fetch("https://api.adviceslip.com/advice");
-  const json = await response.json();
-  console.log(json);
-  ad_title.innerText = `Advice #${json.slip.id}`;
-  ad_body.innerText = `"${json.slip.advice}"`;
+  try {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
+    }
+    const json = await response.json();
+    console.log(json);
+    ad_title.innerText = `Advice #${json.slip.id}`;
+    ad_body.innerText = `"${json.slip.advice}"`;
+  } catch (error) {
+    console.error("Fetch error:", error);
+  }
 }
 dice.addEventListener("click", fetchData);
